70f66f11da5fd4b1b67b629e0e783564f47218ed
Minsu An <minsuan@Minsuui-MacBook-Pro.local>
2019-09-03 10:39:31 +0900

    수정

--- MineSweeper/MineSweeper.vue
+++ MineSweeper/MineSweeper.vue
@@ -9,7 +9,7 @@
 
 <script>
   import { mapState } from 'vuex';
-  import store from './store';
+  import store, { INCREMENT_TIMER } from './store';
   import TableComponent from './TableComponent';
   import MineForm from './MineForm';
   
@@ -25,6 +25,17 @@
     },
     methods: {
     },
+   watch: {
+      halted(value, oldValue) {
+        if (value === false) { // false일 때 게임 시작
+          interval = setInterval(() => {
+            this.$store.commit(INCREMENT_TIMER);
+          }, 1000);
+        } else { // 게임 중단
+          clearInterval(interval);
+        }
+      },
+   }
 
   };
 </script>
--- MineSweeper/store.js
+++ MineSweeper/store.js
@@ -81,22 +81,35 @@ export default new Vuex.Store({ // import store from './store';
       state.result = '';
     },
     [OPEN_CELL](state, { row, cell }) {
-      
+      Vue.set(state.tableData[row], cell, counted.length);
     },
     [CLICK_MINE](state, { row, cell }) {
-     
+      state.halted = true;
+      Vue.set(state.tableData[row], cell, CODE.CLICKED_MINE);
     },
     [FLAG_CELL](state, { row, cell }) {
-     
+      if (state.tableData[row][cell] === CODE.MINE) {
+        Vue.set(state.tableData[row], cell, CODE.FLAG_MINE);
+      } else {
+        Vue.set(state.tableData[row], cell, CODE.FLAG);
+      }
     },
     [QUESTION_CELL](state, { row, cell }) {
-    
+      if (state.tableData[row][cell] === CODE.FLAG_MINE) {
+        Vue.set(state.tableData[row], cell, CODE.QUESTION_MINE);
+      } else {
+        Vue.set(state.tableData[row], cell, CODE.QUESTION);
+      }
     },
     [NORMALIZE_CELL](state, { row, cell }) {
-     
+      if (state.tableData[row][cell] === CODE.QUESTION_MINE) {
+        Vue.set(state.tableData[row], cell, CODE.MINE);
+      } else {
+        Vue.set(state.tableData[row], cell, CODE.NORMAL);
+      }
     },
     [INCREMENT_TIMER](state) {
-     
+     state.timer += 1;
     },
   }, // state를 수정할 때 사용해요. 동기적으로
 });
